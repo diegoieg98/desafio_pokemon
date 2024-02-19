54f933af1088a1236418183f9eb31c65e10673cb
Diego Eguiluz <d.ignacio.eg@gmail.com>
2024-02-19 16:40:22 -0300

    actualizacion desafio

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,7 +3,7 @@ const Home = () =>{
     return(
         <div className="home">
             <h1>Bienvenido maestro pokemon</h1>
-            <img src='/src/assets/img/pikachuu.png' alt="" width='350px' height='350px' />
+            <img src='/src/assets/img/pikachuu.png' alt="pikachu.png" width='350px' height='350px' />
         </div>
     )
 }
